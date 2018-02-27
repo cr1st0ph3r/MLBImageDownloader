fa0074f79bdad5401cdc5529edf0895f95a2727a
cr1st0ph3r <cr1st0ph3r@live.com>
2018-02-27 13:51:56 -0300

    Add files via upload

---
+++ MLBImageDowloader/MainForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace MLBImageDowloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.pbImg1 = new System.Windows.Forms.PictureBox();
+            this.pbImg2 = new System.Windows.Forms.PictureBox();
+            this.pbImg3 = new System.Windows.Forms.PictureBox();
+            this.pbImg4 = new System.Windows.Forms.PictureBox();
+            this.pbImg5 = new System.Windows.Forms.PictureBox();
+            this.pbImg6 = new System.Windows.Forms.PictureBox();
+            this.pbImg7 = new System.Windows.Forms.PictureBox();
+            this.pbImg8 = new System.Windows.Forms.PictureBox();
+            this.pbImg9 = new System.Windows.Forms.PictureBox();
+            this.pbImg10 = new System.Windows.Forms.PictureBox();
+            this.btnSaveImages = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(45, 16);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(628, 22);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.ItemHeight = 16;
+            this.lbLog.Location = new System.Drawing.Point(12, 413);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(895, 116);
+            this.lbLog.TabIndex = 2;
+            // 
+            // pbImg1
+            // 
+            this.pbImg1.Location = new System.Drawing.Point(12, 51);
+            this.pbImg1.Name = "pbImg1";
+            this.pbImg1.Size = new System.Drawing.Size(175, 175);
+            this.pbImg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg1.TabIndex = 3;
+            this.pbImg1.TabStop = false;
+            this.pbImg1.Tag = "";
+            this.pbImg1.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg1.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg2
+            // 
+            this.pbImg2.Location = new System.Drawing.Point(193, 51);
+            this.pbImg2.Name = "pbImg2";
+            this.pbImg2.Size = new System.Drawing.Size(175, 175);
+            this.pbImg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg2.TabIndex = 3;
+            this.pbImg2.TabStop = false;
+            this.pbImg2.Tag = "";
+            this.pbImg2.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg2.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg3
+            // 
+            this.pbImg3.Location = new System.Drawing.Point(374, 51);
+            this.pbImg3.Name = "pbImg3";
+            this.pbImg3.Size = new System.Drawing.Size(175, 175);
+            this.pbImg3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg3.TabIndex = 3;
+            this.pbImg3.TabStop = false;
+            this.pbImg3.Tag = "";
+            this.pbImg3.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg3.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg4
+            // 
+            this.pbImg4.Location = new System.Drawing.Point(555, 51);
+            this.pbImg4.Name = "pbImg4";
+            this.pbImg4.Size = new System.Drawing.Size(175, 175);
+            this.pbImg4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg4.TabIndex = 3;
+            this.pbImg4.TabStop = false;
+            this.pbImg4.Tag = "";
+            this.pbImg4.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg4.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg5
+            // 
+            this.pbImg5.Location = new System.Drawing.Point(736, 51);
+            this.pbImg5.Name = "pbImg5";
+            this.pbImg5.Size = new System.Drawing.Size(175, 175);
+            this.pbImg5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg5.TabIndex = 3;
+            this.pbImg5.TabStop = false;
+            this.pbImg5.Tag = "";
+            this.pbImg5.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg5.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg6
+            // 
+            this.pbImg6.Location = new System.Drawing.Point(12, 232);
+            this.pbImg6.Name = "pbImg6";
+            this.pbImg6.Size = new System.Drawing.Size(175, 175);
+            this.pbImg6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg6.TabIndex = 3;
+            this.pbImg6.TabStop = false;
+            this.pbImg6.Tag = "";
+            this.pbImg6.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg6.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg7
+            // 
+            this.pbImg7.Location = new System.Drawing.Point(193, 232);
+            this.pbImg7.Name = "pbImg7";
+            this.pbImg7.Size = new System.Drawing.Size(175, 175);
+            this.pbImg7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg7.TabIndex = 3;
+            this.pbImg7.TabStop = false;
+            this.pbImg7.Tag = "";
+            this.pbImg7.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg7.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg8
+            // 
+            this.pbImg8.Location = new System.Drawing.Point(374, 232);
+            this.pbImg8.Name = "pbImg8";
+            this.pbImg8.Size = new System.Drawing.Size(175, 175);
+            this.pbImg8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg8.TabIndex = 3;
+            this.pbImg8.TabStop = false;
+            this.pbImg8.Tag = "";
+            this.pbImg8.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg8.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg9
+            // 
+            this.pbImg9.Location = new System.Drawing.Point(555, 232);
+            this.pbImg9.Name = "pbImg9";
+            this.pbImg9.Size = new System.Drawing.Size(175, 175);
+            this.pbImg9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg9.TabIndex = 3;
+            this.pbImg9.TabStop = false;
+            this.pbImg9.Tag = "";
+            this.pbImg9.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg9.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // pbImg10
+            // 
+            this.pbImg10.Location = new System.Drawing.Point(736, 232);
+            this.pbImg10.Name = "pbImg10";
+            this.pbImg10.Size = new System.Drawing.Size(175, 175);
+            this.pbImg10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg10.TabIndex = 3;
+            this.pbImg10.TabStop = false;
+            this.pbImg10.Tag = "";
+            this.pbImg10.Click += new System.EventHandler(this.pbImg_Click);
+            this.pbImg10.Paint += new System.Windows.Forms.PaintEventHandler(this.pbImg_Paint);
+            // 
+            // btnSaveImages
+            // 
+            this.btnSaveImages.Location = new System.Drawing.Point(679, 10);
+            this.btnSaveImages.Name = "btnSaveImages";
+            this.btnSaveImages.Size = new System.Drawing.Size(232, 35);
+            this.btnSaveImages.TabIndex = 4;
+            this.btnSaveImages.Text = "Salvar Imagens Selecionadas";
+            this.btnSaveImages.UseVisualStyleBackColor = true;
+            this.btnSaveImages.Click += new System.EventHandler(this.btnSaveImages_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 537);
+            this.Controls.Add(this.btnSaveImages);
+            this.Controls.Add(this.pbImg10);
+            this.Controls.Add(this.pbImg5);
+            this.Controls.Add(this.pbImg9);
+            this.Controls.Add(this.pbImg4);
+            this.Controls.Add(this.pbImg8);
+            this.Controls.Add(this.pbImg3);
+            this.Controls.Add(this.pbImg7);
+            this.Controls.Add(this.pbImg2);
+            this.Controls.Add(this.pbImg6);
+            this.Controls.Add(this.pbImg1);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.PictureBox pbImg1;
+        private System.Windows.Forms.PictureBox pbImg2;
+        private System.Windows.Forms.PictureBox pbImg3;
+        private System.Windows.Forms.PictureBox pbImg4;
+        private System.Windows.Forms.PictureBox pbImg5;
+        private System.Windows.Forms.PictureBox pbImg6;
+        private System.Windows.Forms.PictureBox pbImg7;
+        private System.Windows.Forms.PictureBox pbImg8;
+        private System.Windows.Forms.PictureBox pbImg9;
+        private System.Windows.Forms.PictureBox pbImg10;
+        private System.Windows.Forms.Button btnSaveImages;
+    }
+}
+
---
+++ MLBImageDowloader/MainForm.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace MLBImageDowloader
+{
+    public partial class MainForm : Form
+    {
+
+        #region Variables
+        string Path = AppDomain.CurrentDomain.BaseDirectory;
+        private string Id = "";
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void reset()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(PictureBox))
+                {
+                    PictureBox pb = c as PictureBox;
+                    if (pb.Tag.ToString().Contains("selected"))
+                    {
+                        pb.Image = null;
+                        pb.Tag = "";
+                    }
+                }
+
+            }
+            Id = "";
+            txtUrl.Text = "";
+            this.Invalidate();
+        }
+
+        /// <summary>
+        /// Devolve a resposta da URL em formato JSON
+        /// *nao requer token*
+        /// </summary>
+        /// <param name="URL"></param>
+        /// <returns></returns>
+        public static JObject getJsonResultFromUrl(string URL)
+        {
+            string html = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "application/json";
+            request.Headers.Add("x-format-new", "true");
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    return JObject.Parse(reader.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public void parseData(string url)
+        {
+            Match match = Regex.Match(url, @"[a-zA-Z]{3}\-\d{9}");
+            if (match.Success)
+            {
+                Id = match.Captures[0].Value.Replace("-", "");
+                var json = getJsonResultFromUrl("https://api.mercadolibre.com/items/"+Id);
+                JArray pics =JArray.Parse( json["pictures"].ToString());
+                List<string> imgs = new List<string>();
+
+                foreach (var item in pics)
+                {
+                    imgs.Add((string)item["secure_url"]);
+                    PictureBox pic = (PictureBox)Controls["pbImg"+imgs.Count];
+                    pic.Load((string)item["secure_url"]);
+                    pic.Tag = "selected";
+                }
+
+                lbLog.Items.Add(imgs.Count + " Imagens Encontradas");
+            }
+
+           
+  
+            //[a-zA-Z]{3}\-\d{9}
+        }
+
+        #region Events
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(this.txtUrl.Text, UriKind.Absolute))
+            {
+                parseData(this.txtUrl.Text);
+            }
+        }
+        private void pbImg_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            if (pb.Tag.ToString().Contains("selected"))
+            {
+                ControlPaint.DrawBorder(e.Graphics, pb.ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+            }
+        }
+
+        private void pbImg_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            if (pb.Tag.ToString().Contains("selected"))
+            {
+                pb.Tag = "";
+            }
+            else
+            {
+                pb.Tag = "selected";
+            }
+            pb.Invalidate();
+        }
+
+        private void btnSaveImages_Click(object sender, EventArgs e)
+        {
+            lbLog.Items.Add("Salvando imagens em :");
+            lbLog.Items.Add(Path+Id);
+
+            bool exists = System.IO.Directory.Exists(Path+Id);
+
+            if (!exists)
+                System.IO.Directory.CreateDirectory(Path + Id);
+
+            int i = 1;
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(PictureBox))
+                {
+                    PictureBox pb = c as PictureBox;
+                    if (pb.Tag.ToString().Contains("selected"))
+                    {
+                        pb.Image.Save(Path + Id +"/"+ i+".jpeg", ImageFormat.Jpeg);
+                        i++;
+                    }
+                }
+              
+            }
+            lbLog.Items.Add(i+" Imagens foram salvas");
+            reset();
+        }
+        #endregion
+
+
+    }
+}
